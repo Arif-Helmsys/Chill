1174ca83aa47963bcc72381f2cd4541f8055af68
Arif-Helmsys <84701901+Arif-Helmsys@users.noreply.github.com>
2022-03-16 13:28:44 +0300

    Update brute.py

--- Simple_BruteForce/brute.py
+++ Simple_BruteForce/brute.py
@@ -2,18 +2,18 @@ import itertools
 import os
 import time
 
-PURPLE = '\033[95m'
-CYAN = '\033[96m'
-DARKCYAN = '\033[36m'
-BLUE = '\033[94m'
-GREEN = '\033[92m'
-YELLOW = '\033[93m'
-RED = '\033[91m'
-BOLD = '\033[1m'
+PURPLE    = '\033[95m'
+CYAN      = '\033[96m'
+DARKCYAN  = '\033[36m'
+BLUE      = '\033[94m'
+GREEN     = '\033[92m'
+YELLOW    = '\033[93m'
+RED       = '\033[91m'
+BOLD      = '\033[1m'
 UNDERLINE = '\033[4m'
-END = '\033[0m'
+END       = '\033[0m'
 
-reset = END
+reset     = END
 
 print(f"""{PURPLE}{BOLD}
                     SIMPLE
